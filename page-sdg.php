dc997e72453ef8438397608cd0dbbf844c77fae8
GFW Jim <jimuel.gfw@gmail.com>
2024-11-20 17:41:02 +0800

    added sdg page

--- page-sdg.php
+++ page-sdg.php
@@ -1,3 +1,20 @@
 <?php get_header() ?>
 
+<section id="sdgPage" class="header-height sdgPage">
+  <section class="landing-sdg">
+    <div class="sdg-container">
+      <div class="global-width">
+        <div class="landing-content">
+          <h1>RESのSDGs<br>甲子園とは</h1>
+          <p>
+            新学習指導要綱に記載されている「持続可能な社会の創り手」を育成するために、地域社会のSDGs課題に焦点を当て、その解決に向けて研究や活動を行う高校生たちが、その取り組みの経過や成果をプレゼンテーション等で競う場を提供します。私たちが求めるのは単なる知識の獲得や表彰の場ではありません。ESD（持続可能な開発のための教育）実践の場として、「世代や立場を超えた交流」「一緒に取り組む仲間との交流」「自ら考え行動する大切さ」「行動（実体験）による学び」「情報発信と新たな関係の構築」の経験やその価値観を深める機会を通じて、未来を担う若者たちの成長を促します。します
+          </p>
+        </div>
+        <div class="landing-image"><img src="<?= RESNPO_IMAGE . '/sdg/sdg-landing.png' ?>" alt="sdg-landing"></div>
+
+      </div>
+    </div>
+  </section>
+</section>
+
 <?php get_footer() ?>
